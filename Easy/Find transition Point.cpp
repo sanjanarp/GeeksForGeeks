5b07e739c94618b721066386f6e1477805e8330d
Sanjana RP <sanjurps100@gmail.com>
2021-08-30 11:53:43 +0530

    Added a new solution

--- Easy/Find transition Point.cpp
+++ Easy/Find transition Point.cpp
@@ -49,3 +49,15 @@ int transitionPoint(int arr[], int n) {
     }
     return -1;
 }
+
+
+//Solution 2: using linear search
+
+int transitionPoint(int arr[], int n) {
+    // code here
+    for(int i=0;i<n;i++){
+        if(arr[i] == 1)
+          return i;
+    }
+    return -1;
+}
